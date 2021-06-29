5142179a06b20bc43c2d2968c128eaee185f8076
Alekhya <51146462+PAlekhya1759@users.noreply.github.com>
2021-06-29 16:25:11 +0530

    Create users-data.js
    Dump data to create new users

---
+++ users-data.js
@@ -0,0 +1,24 @@
+    [{
+      "firstName" : "Alekhya",
+      "middleName" : "",
+      "lastName" : "P",
+      "email" : "alekhya@gmail.com",
+      "mobile" : "9797979797",
+      "password" : "password",
+      "username" : "alekhya@yahoo.com",
+      "deleted" :false
+   }, {
+      "firstName" : "firstName",
+      "middleName" : "middleName",
+      "lastName" : "lastName",
+      "email" : "email@gmail.com",
+      "mobile" : "2222222222",
+      "password" : "password",
+      "username" : "username@gmail.com",
+      "deleted" :false
+   }, {
+      "firstName" : "AlekhyaP",
+      "password" : "password123",
+      "username" : "alekhya@outlook.com",
+      "deleted" :false
+   }];
