09a5cabd69819bfdf245112fc21395ad9c797970
Alekhya <51146462+PAlekhya1759@users.noreply.github.com>
2021-06-29 16:28:29 +0530

    Create task-data.js
    Dump data to create tasks.

---
+++ task-data.js
@@ -0,0 +1,31 @@
+[{
+    "title" : "Task-1",
+    "summary" : "Task-Summary",
+    "type" : "Epic",
+    "reporter" : "60d9e57a9213fe2f786726a3", //This should be created user id
+    "assignee" : "60d9e4d2e74ec740e4063d21", ///This should be created user id
+    "currentStatus" : "ToDo",
+    "priority" : "Highest",
+    "project" : "Project-1",
+     "user": "60d9e57a9213fe2f786726a3", //This should be created user id
+}, {
+    "title" : "Task-2",
+    "summary" : "Task-2-Summary",
+    "type" : "Task",
+    "reporter" : "60d9e57a9213fe2f786726a3", //This should be created user id
+    "assignee" : "60d9e4d2e74ec740e4063d21", ///This should be created user id
+    "currentStatus" : "ToDo",
+    "priority" : "Highest",
+    "project" : "Project-1",
+    "user": "60d9e57a9213fe2f786726a3", //This should be created user id
+} , {
+    "title" : "Task-3",
+    "summary" : "Task-3-Summary",
+    "type" : "Epic",
+    "reporter" : "60d9e57a9213fe2f786726a3", //This should be created user id
+    "assignee" : "60d9e4d2e74ec740e4063d21", ///This should be created user id
+    "currentStatus" : "ToDo",
+    "priority" : "Highest",
+    "project" : "Project-2",
+    "user": "60d9e57a9213fe2f786726a3", //This should be created user id
+}];
